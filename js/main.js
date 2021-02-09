54bc7b5e70bf185a0bf732736a008fc7bab2a633
nick_nov <nov2nik@mail.ru>
2021-02-09 12:10:31 +0500

    Все в корзину добавляется - удаляется (по одному тоже) API - localhost - спасибо с поиском -  второй массив в голову не пришел пытался  с меняющейся переменной - времени жалко!(это тот который с учетки жены)

--- js/main.js
+++ js/main.js
@@ -1,25 +1,86 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 20000},
-    {id: 2, title: 'Mouse', price: 1500},
-    {id: 3, title: 'Keyboard', price: 5000},
-    {id: 4, title: 'Gamepad', price: 4500},
-];
+const API = 'http://localhost/project/';
 
-const renderProduct = (title, price, img = '') => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="by-btn">Добавить в корзину</button>
-              </div>`;
-};
+const app = new Vue({
+	el: '#app',
+	data: {
+		userSearch: '',
+        showCart: false,
+		catalogUrl: 'catalogData.json',
+		products: [],
+		cart: [],
+		filtered: [],
+		imgCatalog: 'https://placehold.it/200x150',
+		show: false,
 
-const renderProducts = (list) => {
-    const productList = list.map(function (product) {
-        return renderProduct(product.title, product.price);
-    });
-    console.log(productList);
-    document.querySelector('.products').innerHTML = productList;
-    // insertAdjacentHTML();
-};
 
-renderProducts(products);
+	},
+	methods: {
+		getJson(url) {
+			return fetch(url)
+				.then(result => result.json())
+				.catch(error => {
+					console.log(error);
+				})
+		},
+		addProduct(product) {
+			for (rec of this.cart) {
+				if (rec.product_name == product.product_name) {
+					rec.quantity++;
+					return;
+				}
+			}
+			this.cart.push({...product, quantity:1});
+
+		},
+		deletePositionCart(item) {
+			for (rec of this.cart) {
+				if (rec.product_name == item.product_name && rec.quantity > 1) {
+					rec.quantity--;
+					return;
+				}
+			}
+			console.log(rec);
+			let index = this.cart.indexOf(item);
+			this.cart.splice(index, 1);
+		},
+		filter() {
+      let regexp = new RegExp(this.userSearch, 'i');
+      this.filtered = this.products.filter(el => regexp.test(el.product_name));
+    },
+
+	},
+	computed: {
+
+	},
+	beforeCreate() {
+
+	},
+	created() {
+		this.getJson(`${API + this.catalogUrl}`)
+			.then(data => {
+				for (let el of data) {
+                this.products.push(el);
+                this.filtered.push(el);
+              }
+			});
+	},
+
+	beforeMount() {
+
+	},
+	mounted() {
+
+	},
+	beforeUpdate() {
+
+	},
+	updated() {
+
+	},
+	beforeDestroy() {
+
+	},
+	destroyed() {
+
+	},
+});
